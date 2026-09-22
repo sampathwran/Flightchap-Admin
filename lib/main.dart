52f2e864f236538f36dea1dd608c2266119063bd
Sampathwran <sampathwran@gmail.com>
2026-09-23 00:39:36 +0530

    feat: bypass login screen

--- lib/main.dart
+++ lib/main.dart
@@ -75,6 +75,13 @@ class AuthWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Bypassing login screen
+    return const MainLayout();
+  }
+});
+
+  @override
+  Widget build(BuildContext context) {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
